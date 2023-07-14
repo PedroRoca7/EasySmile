2de8c47fa7cbfcb1540de6ac487929a605895d2b
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-07-14 18:09:59 -0300

    Commit criado tela de login

---
+++ EasySmile/Views/ScreenLoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ScreenLoginViewController.swift
+//  EasySmile
+//
+//  Created by Pedro Henrique on 14/07/23.
+//
+
+import UIKit
+
+class ScreenLoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var senhaTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+    
+    @IBAction func forgotPassword(_ sender: Any) {
+    }
+    
+    @IBAction func needHelp(_ sender: Any) {
+    }
+    
+}
