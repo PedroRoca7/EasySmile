89b2846f0f91d091407b8cec5d95454f1506c998
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-07-18 13:28:38 -0300

    Commit finalizado o scrollView
    Finalizado de implementar o ScrollView na tela de cadastro do Odontologista

---
+++ EasySmile/Views/RegisterDentistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterDentistViewController.swift
+//  EasySmile
+//
+//  Created by Pedro Henrique on 18/07/23.
+//
+
+import UIKit
+
+class RegisterDentistViewController: UIViewController {
+
+    @IBOutlet weak var nomeCompletoDentistTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cpfTextField: UITextField!
+    @IBOutlet weak var telefoneTextField: UITextField!
+    @IBOutlet weak var numeroDaInscricaoConselhoTextField: UITextField!
+    @IBOutlet weak var ruaDoConsultorioTextField: UITextField!
+    @IBOutlet weak var senhaTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ EasySmile/Views/RegisterPatientViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RegisterPatientViewController.swift
+//  EasySmile
+//
+//  Created by Pedro Henrique on 18/07/23.
+//
+
+import UIKit
+
+class RegisterPatientViewController: UIViewController {
+
+    @IBOutlet weak var nomeCompletoPacientTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cpfTextField: UITextField!
+    @IBOutlet weak var telefoneTextField: UITextField!
+    @IBOutlet weak var senhaTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cadastrarPaciente(_ sender: Any) {
+    }
+    
+    
+}
