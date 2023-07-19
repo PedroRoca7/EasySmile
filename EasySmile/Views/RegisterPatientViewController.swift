0991a398fc545fccc37f8ea0856b8d58201c4407
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-07-18 22:09:18 -0300

    Commit criado integração com o firebase
    Criado a integração com o firebase do cadastro do Paciente.

--- EasySmile/Views/RegisterPatientViewController.swift
+++ EasySmile/Views/RegisterPatientViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+
+
 
 class RegisterPatientViewController: UIViewController {
 
@@ -14,13 +18,75 @@ class RegisterPatientViewController: UIViewController {
     @IBOutlet weak var cpfTextField: UITextField!
     @IBOutlet weak var telefoneTextField: UITextField!
     @IBOutlet weak var senhaTextField: UITextField!
+    @IBOutlet weak var cadastrarButton: UIButton!
+    
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addObservadoresTextField()
+        
+    }
+    
+    func addObservadoresTextField() {
+        nomeCompletoPacientTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        emailTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        cpfTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        telefoneTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        senhaTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange() {
+        // Verificar se todos os campos estão preenchidos para habilitar/desabilitar o botão de envio
+        let textFields: [UITextField] = [nomeCompletoPacientTextField, emailTextField, cpfTextField, telefoneTextField, senhaTextField]
 
+        var allFieldsFilled = true
+
+        for textField in textFields {
+            if let text = textField.text, text.isEmpty {
+                allFieldsFilled = false
+                break
+            }
+        }
+        
+        if allFieldsFilled {
+            cadastrarButton.isEnabled = true
+            cadastrarButton.backgroundColor = .magenta
+        } else {
+            cadastrarButton.isEnabled = false
+            cadastrarButton.backgroundColor = .darkGray
+        }
+    }
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        // Realizar a ação do botão de envio aqui
+        print("Botão de envio pressionado!")
     }
 
     @IBAction func cadastrarPaciente(_ sender: Any) {
+        
+        guard let nome = nomeCompletoPacientTextField.text,
+              let email = emailTextField.text,
+              let cpf = cpfTextField.text,
+              let telefone = telefoneTextField.text,
+              let senha = senhaTextField.text else { return }
+        
+        let usuarioData: [String: Any] = [
+            "nome": nome,
+            "email": email,
+            "cpf": cpf,
+            "telefone": telefone,
+            "senha": senha
+        ]
+        
+        db.collection("Usuarios").addDocument(data: usuarioData) { error in
+            if let error = error {
+                print("Erro ao cadastrar Usuário \(error.localizedDescription)")
+            } else {
+                print("Usuário cadastrado com sucesso")
+            }
+        }
+        
     }
     
     
--- EasySmile/Views/ScreenLoginViewController.swift
+++ EasySmile/Views/ScreenLoginViewController.swift
@@ -15,8 +15,7 @@ class ScreenLoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+ 
     }
     
     @IBAction func loginPressed(_ sender: Any) {
